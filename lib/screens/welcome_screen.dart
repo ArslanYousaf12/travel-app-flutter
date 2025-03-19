3760da0f70a3286ee334a9a44df6d48f5bff1238
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-19 15:10:02 +0500

    Add AppLargeText and AppText widgets for consistent text styling

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
 
 class WeclomeScreen extends StatefulWidget {
   const WeclomeScreen({super.key});
@@ -23,6 +24,14 @@ class _WeclomeScreenState extends State<WeclomeScreen> {
               image: DecorationImage(
                 image: AssetImage("assets/images/" + images[index]),
                 fit: BoxFit.cover,
+              ),
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+              child: Row(
+                children: [
+                  Column(children: [AppLargeText(text: "Trips")]),
+                ],
               ),
             ),
           );
