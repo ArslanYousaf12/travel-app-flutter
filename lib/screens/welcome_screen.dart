601b2f8e13cd6c43f042e8e6d0d8bc3ece282171
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-20 12:39:35 +0500

    Add responsive button and enhance welcome screen layout with additional text

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
 import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
+import 'package:travel_app/widgets/responsive_button.dart';
 
 class WeclomeScreen extends StatefulWidget {
   const WeclomeScreen({super.key});
@@ -30,7 +33,25 @@ class _WeclomeScreenState extends State<WeclomeScreen> {
               margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
               child: Row(
                 children: [
-                  Column(children: [AppLargeText(text: "Trips")]),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(text: "Mountain", size: 30),
+                      SizedBox(height: 20),
+                      Container(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredible sense of freedom along with endurance test",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      ResponsiveButton(),
+                    ],
+                  ),
                 ],
               ),
             ),
---
+++ lib/widgets/responsive_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+
+class ResponsiveButton extends StatelessWidget {
+  const ResponsiveButton({super.key, this.width, this.isResponsive = false});
+  final bool isResponsive;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor,
+      ),
+      child: Row(children: [Image.asset("assets/images/button-one.png")]),
+    );
+  }
+}
