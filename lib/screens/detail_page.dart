bf013807937516b0ba5bea3a0b51b57129e432ba
Arslan Yousaf <devtechofficial90@gmail.com>
2025-03-23 16:45:28 +0500

    Add DetailPage layout and AppButton widget for improved UI

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/misc/colors.dart';
+import 'package:travel_app/widgets/app_button.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -8,6 +12,7 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gottenStar = 4;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +46,95 @@ class _DetailPageState extends State<DetailPage> {
                     onPressed: () {},
                   ),
                 ],
+              ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                height: 500,
+                width: MediaQuery.of(context).size.width,
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                          text: "Yosemite",
+                          color: Colors.black.withOpacity(0.9),
+                        ),
+                        AppLargeText(
+                          text: "\$ 230",
+                          color: AppColors.mainColor,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(Icons.location_on, color: AppColors.mainColor),
+                        SizedBox(width: 5),
+                        AppText(
+                          text: "USA, California",
+                          color: AppColors.textColor1,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color:
+                                  index < gottenStar
+                                      ? AppColors.starColor
+                                      : AppColors.textColor2,
+                            );
+                          }),
+                        ),
+                        AppText(text: "(4.0)", color: AppColors.textColor2),
+                      ],
+                    ),
+                    SizedBox(height: 25),
+                    AppLargeText(
+                      text: "People",
+                      color: Colors.black.withOpacity(0.8),
+                      size: 20,
+                    ),
+                    SizedBox(height: 5),
+                    AppText(text: "Number of Peoples in your group"),
+                    SizedBox(height: 10),
+
+                    Wrap(
+                      children: List.generate(5, (index) {
+                        return InkWell(
+                          onTap: () {},
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            child: AppButton(
+                              color: Colors.black,
+                              backgroundColor: AppColors.buttonBackground,
+                              borderColor: AppColors.buttonBackground,
+                              size: 50,
+                              text: (index + 1).toString(),
+                            ),
+                          ),
+                        );
+                      }),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
